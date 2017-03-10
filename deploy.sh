3972f631faa024f2d9302a8e50654b559794031f
sidddy <mail@svenhenkel.de>
2017-03-10 09:04:03 +0100

    Deployment fixes & prod enablement

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
 ERRORSTRING="Error. Please make sure you've indicated correct parameters"
+RSYNC_PARAM="-az --force --progress --delete --exclude-from=rsync_exclude.txt -e ssh ./"
 
 if [ $# -eq 0 ]
     then
         echo $ERRORSTRING;
-elif [ $1 == "local" ]
-    then
+else
+    TARGET="none";
+    if [ $1 == "local" ]; then
+        TARGET="localhost";
+    elif [ $1 == "prod" ]; then
+        TARGET="rp2";
+    fi
+    if [ $TARGET != "none" ]; then
         if [[ -z $2 ]]
             then
                 echo "Running dry-run"
-                rsync --dry-run -az --force --delete --progress --exclude-from=rsync_exclude.txt -e "ssh -p22" ./ sven@apollon:/var/www/runalyze
+                rsync --dry-run $RSYNC_PARAM sven@$TARGET:/var/www/runalyze
         elif [ $2 == "go" ]
             then
                 echo "Running actual deploy"
-                rsync -az --force --progress --delete --exclude-from=rsync_exclude.txt -e "ssh -p22" ./ sven@apollon:/var/www/runalyze
+                rsync $RSYNC_PARAM sven@$TARGET:/var/www/runalyze
         else
             echo $ERRORSTRING;
         fi
+    else
+	echo "Unknown target. \"local\" and \"prod\" are known."
+    fi
 fi
