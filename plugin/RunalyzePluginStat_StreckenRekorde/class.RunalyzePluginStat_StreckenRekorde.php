42333fd503542f01451c92ebceaca9aed0662e14
sidddy <mail@svenhenkel.de>
2017-08-03 15:48:15 +0200

    Segment records: Add reference activity to header, show only segments that relate to the reference act, show diff times for each activity (either to ref or to best)

--- plugin/RunalyzePluginStat_StreckenRekorde/class.RunalyzePluginStat_StreckenRekorde.php
+++ plugin/RunalyzePluginStat_StreckenRekorde/class.RunalyzePluginStat_StreckenRekorde.php
@@ -25,6 +25,7 @@ define("MAX_TIME", 999999);
 class RunalyzePluginStat_StreckenRekorde extends PluginStat {
 	private $SegementRecordData = array();
 	private $counter = 0;
+	private $activityRef = -1;
 
 
 	/**
@@ -48,19 +49,117 @@ class RunalyzePluginStat_StreckenRekorde extends PluginStat {
 	 * @return int
 	 */
 	protected function defaultYear() {
-		return -1;
+		return 12;
 	}
 
 	/**
 	 * Init data 
 	 */
 	protected function prepareForDisplay() {
+		if (isset($_GET['ref']))                                                                                                                                     
+        	if (is_numeric($_GET['ref']))                                                                                                                        
+				$this->activityRef = $_GET['ref'];
+		
 		$this->setSportsNavigation(true, false);
 		$this->setYearsNavigation(true, true, true);
 
 		$this->setHeaderWithSportAndYear();
 
 		$this->initSegmentRecordData();
+		$this->initActivityLinks();
+	}
+	
+	/**                                                                                                                                                                    
+     * Returns the html-link for inner-html-navigation                                                                                                                     
+     * @param string $name displayed link-name                                                                                                                             
+     * @param int $sport id of sport, default $this->sportid                                                                                                               
+     * @param int $year year, default $this->year                                                                                                                          
+     * @param string $dat optional dat-parameter                                                                                                                           
+     * @return string                                                                                                                                                      
+     */                                                                                                                                                                    
+    protected function getInnerLink($name, $sport = 0, $year = 0, $dat = '', $ref = 0) {
+            if ($sport == 0) {                                                                                                                                             
+                    $sport = $this->sportid;                                                                                                                               
+            }                                                                                                                                                              
+                                                                                                                                                                           
+            if ($year == 0) {                                                                                                                                              
+                    $year = $this->year;                                                                                                                                   
+            }                                  
+            
+            if ($ref == 0) {
+            		$ref = $this->activityRef;
+            }
+                                                                                                                                                                           
+            return Ajax::link($name, 'statistics-inner', self::$DISPLAY_URL.'/'.$this->id().'?sport='.$sport.'&jahr='.$year.'&dat='.$dat.'&ref='.$ref);                                 
+    }
+	
+	protected function getRefString() {
+		if ($this->activityRef < 0) {
+			return 'No Reference';
+		}
+		
+		$Db = DB::getInstance();
+		
+		$activities = $Db->query('
+			SELECT
+				`'.PREFIX.'training`.`id`,
+				`'.PREFIX.'training`.`time`,
+				`'.PREFIX.'training`.`title`
+			FROM `'.PREFIX.'training`
+			WHERE `'.PREFIX.'training`.`accountid`="'.SessionAccountHandler::getId().'" 
+			AND `'.PREFIX.'training`.`id`="'.$this->activityRef.'"
+			AND `'.PREFIX.'training`.`routeid` IS NOT NULL '.$this->getSportAndYearDependenceForQuery().'
+			LIMIT 1'
+		)->fetchAll();
+		
+		foreach ($activities as $act) {
+			$date = (new LocalTime($act["time"]))->format('d.m.Y');
+			$txt = $date.' '.$act["title"];
+			
+			return $txt;
+		}
+		
+		return 'n/a';
+	}
+	
+	protected function getRefLinksAsList() {
+		$list = '';
+		
+		if ($this->activityRef == -1) {
+			$list .= '<li class="active">'.$this->getInnerLink("No reference", 0, 0, '', -1).'</li>';
+		} else {
+			$list .= '<li>'.$this->getInnerLink("No reference", 0, 0, '', -1).'</li>';
+		}
+		
+		$Db = DB::getInstance();
+		
+		$activities = $Db->query('
+			SELECT
+				`'.PREFIX.'training`.`id`,
+				`'.PREFIX.'training`.`time`,
+				`'.PREFIX.'training`.`title`
+			FROM `'.PREFIX.'training`
+			WHERE `'.PREFIX.'training`.`accountid`="'.SessionAccountHandler::getId().'" 
+			AND `'.PREFIX.'training`.`routeid` IS NOT NULL '.$this->getSportAndYearDependenceForQuery().'
+			ORDER BY `'.PREFIX.'training`.`time` DESC
+			LIMIT 20'
+		)->fetchAll();
+		
+		foreach ($activities as $act) {
+			$date = (new LocalTime($act["time"]))->format('d.m.Y');
+			$txt = $date.' '.$act["title"];
+			
+			$list .= '<li>'.$this->getInnerLink($txt, 0, 0, '', $act["id"]).'</li>';
+		}
+		
+		return $list;
+	}
+	
+	
+	protected function initActivityLinks() {
+		$ll = array();
+		$ll[] = '<li class="with-submenu"><span class="link">'.$this->getRefString().'</span><ul class="submenu">'.$this->getRefLinksAsList().'</ul>';
+		$this->setToolbarNavigationLinks($ll);
 	}
 
 
@@ -84,21 +183,47 @@ class RunalyzePluginStat_StreckenRekorde extends PluginStat {
 	}
 	
 	private function displaySegmentRecordData() {
-		echo '<div style="padding-left:30px; padding-right:30px"><div class="seg-tables">';
+		echo '<div style="padding-left:30px; padding-right:30px">';
+		echo '<div class="seg-tables">';
 		foreach ($this->segmentRecordData as $segment) {
 			
-			echo '<div><table class="zebra-style center">';
+			$ref_time = -1;
+			
+			if ($this->activityRef != -1){
+				// do we have an entry for our reference activity??
+				$ref_found = 0;
+				foreach ($segment["records"] as $record) {
+					if ($record["activityid"] == $this->activityRef and $record["time"] < MAX_TIME) {
+						$ref_found = 1;
+						$ref_time = $record["time"];
+						break;
+					}
+				}
+				if ($ref_found == 0) {
+					continue;
+				}
+			}
+			
+			echo '<div><table class="zebra-style center" style="margin-right: auto; margin-left: auto;">';
 			echo '<thead><tr><th colspan="11" class="l">'.$segment["name"].'</th></tr></thead>';
 			echo '<tbody>';
 			$found = 0;
 			foreach ($segment["records"] as $record) {
 				if ($record["time"] < MAX_TIME) {
 					$found = 1;
-					echo '<tr class="r">';
+					if ($ref_time == -1) {
+						$ref_time = $record["time"];
+					}
+					if ($record["activityid"] == $this->activityRef) {
+						echo '<tr class="r highlight">';
+					} else {
+						echo '<tr class="r">';
+					}
 					$date = (new LocalTime($record["start_time"]))->format('d.m.Y');
 					echo '<td class="b l">'.$date.'</td>';
 					echo '<td class="b l">'.Ajax::trainingLink($record["activityid"], $this->labelFor($record["title"])).'</td>';
 					echo '<td class="b l">'.Duration::format($record["time"]).'</td>';
+					echo '<td class="b l">'.($ref_time==$record["time"]?' ':($ref_time>$record["time"]?'- '.Duration::format($ref_time-$record["time"]):'+ '.Duration::format($record["time"]-$ref_time))).'</td>';
 					echo '</tr>';
 				}
 			}
